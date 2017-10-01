5a280c1282eababb30e5b677ed07e025eca7073c
Zach Falcone <zac.falcone@gmail.com>
2017-09-30 17:33:04 -0700

    Added HelloTeam.java.
    Example commit. To be used for checking in/out.

---
+++ HelloTeam.java
@@ -0,0 +1,29 @@
+public class HelloTeam {
+    public static void main(String[] args) {
+        HelloTeam helloTeam = new HelloTeam();
+        helloTeam.start();
+    }
+    public void start() {
+        System.out.println("Hello Team. Our team members are:");
+        
+        TeamMember zach = new TeamMember("Zach", "Programmer");
+        zach.printInfo();
+        
+        // Add your names below
+        
+    }
+    private class TeamMember {
+        private String name, role;
+        TeamMember(String name) {
+            this.name = name;
+            role = "Undecided";
+        }
+        TeamMember(String name, String role) {
+            this.name = name;
+            this.role = role;
+        }
+        void printInfo() {
+            System.out.println(name + " - " + role + ".");
+        }
+    }
+}
